2aed83eda20f6bdbea679c4ed9f621f6b437acd2
Jonathan Tien <jonathan.tien@gmail.com>
2012-10-29 11:19:26 -0700

    initial draft of API file

---
+++ API.rb
@@ -0,0 +1,21 @@
+require 'rubygems'
+require 'sinatra'
+require 'haml'
+# require 'builder'
+
+get '/' do
+	"welcome to hymnal.net (unofficial) API"
+    
+    # HAML ?
+    
+end
+
+get '/hymn/:id' do
+    
+    # parse 
+
+end
+
+get '/search/:string' do
+
+end
